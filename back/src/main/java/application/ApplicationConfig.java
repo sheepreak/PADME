b3f2de1c66cb64c16869d53af51514bca971631f
Charles Da Silva Costa <dasilvacosta.charles@gmail.com>
2018-01-19 22:55:07 +0100

    Add CORS

--- back/src/main/java/application/ApplicationConfig.java
+++ back/src/main/java/application/ApplicationConfig.java
@@ -1,6 +1,8 @@
 package application;
 
 
+import application.filters.CORSFilter;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 import java.util.Collections;
@@ -14,6 +16,7 @@ public class ApplicationConfig extends Application {
     public ApplicationConfig() {
         HashSet<Class<?>> c = new HashSet<>();
         c.add(TestHello.class);
+        c.add(CORSFilter.class);
         classes = Collections.unmodifiableSet(c);
     }
 
