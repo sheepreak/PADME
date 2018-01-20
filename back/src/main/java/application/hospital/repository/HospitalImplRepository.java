b392966cda9005b9fdc7d4c99a8501eb80cf52f3
ducheminkvn <duchemin.kvn@gmail.com>
2018-01-20 10:53:10 +0100

    add 4 interface/class for Hospital Component
    back =>
    * add interface Node (complete)
    * add interface Hospital (complete)
    * add class HospitalImpl (not complete)
    * add class HospitalImplRepository (complete)

---
+++ back/src/main/java/application/hospital/repository/HospitalImplRepository.java
@@ -0,0 +1,44 @@
+package application.hospital.repository;
+
+import application.hospital.Hospital;
+import application.hospital.domain.HospitalImpl;
+
+import javax.ejb.NoSuchEntityException;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class HospitalImplRepository {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<Hospital> list(){
+        List<Hospital> list = new ArrayList<>(entityManager
+                .createNamedQuery(HospitalImpl.FIND_ALL, HospitalImpl.class)
+                .getResultList());
+        return list;
+    }
+
+    public Hospital find(Long id){
+        return entityManager.find(HospitalImpl.class, id);
+    }
+
+    public Long save(HospitalImpl hospital) {
+        entityManager.persist(hospital);
+        return hospital.getId();
+    }
+
+    public void delete(Long id) throws NoSuchEntityException{
+        Hospital hospital = null;
+        hospital = find(id);
+        if(hospital!=null)
+            entityManager.remove(hospital);
+        else throw new NoSuchEntityException();
+    }
+
+    public void update(HospitalImpl hospital){
+        entityManager.merge(hospital);
+    }
+}
