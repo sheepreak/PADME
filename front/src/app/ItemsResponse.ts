af4f2922b1410e6cae57c78405abe1eeee562faa
Daagama <amelinemoreau@orange.fr>
2018-01-23 09:31:29 +0100

    Reception des requètes

---
+++ front/src/app/ItemsResponse.ts
@@ -0,0 +1,4 @@
+
+interface ItemsResponse {
+  results: string[];
+}
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
 import { StaticNavBarComponent } from './static-nav-bar/static-nav-bar.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent,
     StaticNavBarComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
