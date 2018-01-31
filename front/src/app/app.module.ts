763ccff4cd3fe75429a3ffcec90365ac278c4e8c
Charles Da Silva Costa <dasilvacosta.charles@gmail.com>
2018-01-31 18:41:16 +0100

    Change path to patientList

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -29,7 +29,7 @@ const appRoutes: Routes = [
     component: PrescriptionFileComponent
   },
   {
-    path: 'patientlistcomponent',
+    path: 'patientlist',
     component: PatientListComponent
   }
 ];
