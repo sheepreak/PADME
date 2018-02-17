fa610203aef6ec52e01f7fa7f310546d75c4067d
ducheminkvn <duchemin.kvn@gmail.com>
2018-02-17 16:59:26 +0100

    back => Hospital integration test

--- back/src/test/java/test/application/hospital/domain/HospitalIntegrationTest.java
+++ back/src/test/java/test/application/hospital/domain/HospitalIntegrationTest.java
@@ -13,8 +13,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class HospitalIntegrationTest {
 
