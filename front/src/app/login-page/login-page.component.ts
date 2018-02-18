46757e7645cc4685bfe5664c235cf42011869da0
charlesdasilvacosta <dasilvacosta.charles@gmail.com>
2018-02-18 15:50:54 +0100

    Update login page

--- front/src/app/login-page/login-page.component.ts
+++ front/src/app/login-page/login-page.component.ts
@@ -8,7 +8,7 @@ import {Router} from '@angular/router';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  styleUrls: ['./login-page.component.css','./../app.component.css']
 })
 
 
